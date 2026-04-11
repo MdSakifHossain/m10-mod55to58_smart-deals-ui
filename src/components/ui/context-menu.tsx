47e3112385e1ce1dc1f4f2838f798092d8e2c4d2
MdSakifHossain <sakif015sakif@gmail.com>
2026-04-11 22:40:25 +0600

    style: update shadcn/ui theme

--- src/components/ui/context-menu.tsx
+++ src/components/ui/context-menu.tsx
@@ -2,7 +2,7 @@ import * as React from "react"
 import { ContextMenu as ContextMenuPrimitive } from "@base-ui/react/context-menu"
 
 import { cn } from "@/lib/utils"
-import { RiArrowRightSLine, RiCheckLine } from "@remixicon/react"
+import { IconChevronRight, IconCheck } from "@tabler/icons-react"
 
 function ContextMenu({ ...props }: ContextMenuPrimitive.Root.Props) {
   return <ContextMenuPrimitive.Root data-slot="context-menu" {...props} />
@@ -132,7 +132,7 @@ function ContextMenuSubTrigger({
       {...props}
     >
       {children}
-      <RiArrowRightSLine className="ml-auto" />
+      <IconChevronRight className="ml-auto" />
     </ContextMenuPrimitive.SubmenuTrigger>
   )
 }
@@ -172,7 +172,7 @@ function ContextMenuCheckboxItem({
     >
       <span className="pointer-events-none absolute right-2">
         <ContextMenuPrimitive.CheckboxItemIndicator>
-          <RiCheckLine
+          <IconCheck
           />
         </ContextMenuPrimitive.CheckboxItemIndicator>
       </span>
@@ -212,7 +212,7 @@ function ContextMenuRadioItem({
     >
       <span className="pointer-events-none absolute right-2">
         <ContextMenuPrimitive.RadioItemIndicator>
-          <RiCheckLine
+          <IconCheck
           />
         </ContextMenuPrimitive.RadioItemIndicator>
       </span>

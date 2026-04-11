7b3da7bb5e31c6f265c2a33a37c6ecdb6f6d0a07
MdSakifHossain <sakif015sakif@gmail.com>
2026-04-12 00:23:38 +0600

    feat(login): update login page with a professtional look

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,12 +1,104 @@
 // @ts-nocheck
 import PageStructure from "@/components/my-components/PageStructure"
 
-const Login = () => {
+import { cn } from "@/lib/utils"
+import { Button } from "@/components/ui/button"
+import { Card, CardContent } from "@/components/ui/card"
+import {
+  Field,
+  FieldDescription,
+  FieldGroup,
+  FieldLabel,
+  FieldSeparator,
+} from "@/components/ui/field"
+import { Input } from "@/components/ui/input"
+import { IconBrandGoogleFilled, IconSpider } from "@tabler/icons-react"
+
+export default function Login() {
   return (
-    <PageStructure>
-      <p>Login Page</p>
+    <PageStructure className="flex items-center justify-center">
+      <div className="w-full max-w-sm md:max-w-4xl">
+        <LoginForm />
+      </div>
     </PageStructure>
   )
 }
 
-export default Login
+function LoginForm({ className, ...props }) {
+  return (
+    <div className={cn("flex flex-col gap-6", className)} {...props}>
+      <Card className="overflow-hidden p-0">
+        <CardContent className="grid p-0 md:grid-cols-2">
+          <form className="p-6 md:p-8">
+            <FieldGroup>
+              <div className="flex flex-col items-center gap-2 text-center">
+                <h1 className="text-2xl font-bold">Welcome back</h1>
+                <p className="text-balance text-muted-foreground">
+                  Login to your Acme Inc account
+                </p>
+              </div>
+              <Field>
+                <FieldLabel htmlFor="email">Email</FieldLabel>
+                <Input
+                  id="email"
+                  type="email"
+                  placeholder="m@example.com"
+                  required
+                />
+              </Field>
+              <Field>
+                <div className="flex items-center">
+                  <FieldLabel htmlFor="password">Password</FieldLabel>
+                  <a
+                    href="#"
+                    className="ml-auto text-xs underline-offset-2 hover:underline"
+                  >
+                    Forgot your password?
+                  </a>
+                </div>
+                <Input
+                  id="password"
+                  type="password"
+                  placeholder="your password"
+                  required
+                />
+              </Field>
+              <Field>
+                <Button type="submit">Login</Button>
+              </Field>
+              <FieldSeparator className="*:data-[slot=field-separator-content]:bg-card">
+                Or continue with
+              </FieldSeparator>
+              <Field className="grid grid-cols-2 gap-4">
+                <Button variant="outline" type="button">
+                  <IconBrandGoogleFilled />
+                  <span className="sr-only">Login with Google</span>
+                </Button>
+                <Button variant="outline" type="button" disabled>
+                  <IconSpider />
+                  <span className="sr-only">Login with Spider</span>
+                </Button>
+              </Field>
+              <FieldDescription className="text-center">
+                Don&apos;t have an account? <a href="#">Sign up</a>
+              </FieldDescription>
+            </FieldGroup>
+          </form>
+
+          <div className="relative hidden bg-muted md:block">
+            <img
+              src="https://images.unsplash.com/photo-1664455340023-214c33a9d0bd"
+              alt="Image"
+              className="absolute inset-0 h-full w-full object-cover dark:brightness-[0.6]"
+            />
+          </div>
+        </CardContent>
+      </Card>
+
+      <FieldDescription className="px-6 text-center">
+        By clicking continue, you agree to our <a href="#">Terms of Service</a>{" "}
+        and <a href="#">Privacy Policy</a>.
+      </FieldDescription>
+    </div>
+  )
+}

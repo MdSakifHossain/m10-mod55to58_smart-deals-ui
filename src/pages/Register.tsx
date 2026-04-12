bfc9071d6372424100b60d2db3311ea70d59d515
MdSakifHossain <sakif015sakif@gmail.com>
2026-04-12 09:09:33 +0600

    feat: add Register page

---
+++ src/pages/Register.tsx
@@ -0,0 +1,111 @@
+import PageStructure from "@/components/my-components/PageStructure"
+
+export default function Register() {
+  return (
+    <PageStructure className="flex w-full flex-col items-center justify-center p-6 md:p-10">
+      <div className="w-full max-w-sm md:max-w-4xl">
+        <SignupForm />
+      </div>
+    </PageStructure>
+  )
+}
+
+import { cn } from "@/lib/utils"
+import { Button } from "@/components/ui/button"
+import { Card, CardContent } from "@/components/ui/card"
+import {
+  Field,
+  FieldDescription,
+  FieldGroup,
+  FieldLabel,
+  FieldSeparator,
+} from "@/components/ui/field"
+import { Input } from "@/components/ui/input"
+import { IconBrandGoogleFilled, IconSpider } from "@tabler/icons-react"
+
+function SignupForm({ className, ...props }) {
+  return (
+    <div className={cn("flex flex-col gap-6", className)} {...props}>
+      {/* top part */}
+      <Card className="overflow-hidden p-0">
+        <CardContent className="grid p-0 md:grid-cols-2">
+          {/* left part */}
+          <form className="p-6 md:p-8">
+            <FieldGroup>
+              <div className="flex flex-col items-center gap-2 text-center">
+                <h1 className="text-2xl font-bold">Create your account</h1>
+                <p className="text-sm text-balance text-muted-foreground">
+                  Enter your email below to create your account
+                </p>
+              </div>
+              <Field>
+                <FieldLabel htmlFor="email">Email</FieldLabel>
+                <Input
+                  id="email"
+                  type="email"
+                  placeholder="m@example.com"
+                  required
+                />
+                <FieldDescription>
+                  We&apos;ll use this to contact you. We will not share your
+                  email with anyone else.
+                </FieldDescription>
+              </Field>
+              <Field>
+                <Field className="grid grid-cols-2 gap-4">
+                  <Field>
+                    <FieldLabel htmlFor="password">Password</FieldLabel>
+                    <Input id="password" type="password" required />
+                  </Field>
+                  <Field>
+                    <FieldLabel htmlFor="confirm-password">
+                      Confirm Password
+                    </FieldLabel>
+                    <Input id="confirm-password" type="password" required />
+                  </Field>
+                </Field>
+                <FieldDescription>
+                  Must be at least 8 characters long.
+                </FieldDescription>
+              </Field>
+              <Field>
+                <Button type="submit">Create Account</Button>
+              </Field>
+              <FieldSeparator className="*:data-[slot=field-separator-content]:bg-card">
+                Or continue with
+              </FieldSeparator>
+              <Field className="grid grid-cols-2 gap-4">
+                <Button variant="outline" type="button">
+                  <IconBrandGoogleFilled />
+                  <span className="sr-only">Sign up with Google</span>
+                </Button>
+                <Button variant="outline" type="button" disabled>
+                  <IconSpider />
+                  <span className="sr-only">Login with Spider</span>
+                </Button>
+              </Field>
+              <FieldDescription className="text-center">
+                Already have an account? <a href="#">Sign in</a>
+              </FieldDescription>
+            </FieldGroup>
+          </form>
+
+          {/* right part */}
+          <div className="relative hidden bg-muted md:block">
+            <img
+              src="https://images.unsplash.com/photo-1664455340023-214c33a9d0bd"
+              alt="Image"
+              className="absolute inset-0 h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
+            />
+          </div>
+        </CardContent>
+      </Card>
+
+      {/* bottom part */}
+      <FieldDescription className="px-6 text-center">
+        By clicking continue, you agree to our <a href="#">Terms of Service</a>{" "}
+        and <a href="#">Privacy Policy</a>.
+      </FieldDescription>
+    </div>
+  )
+}
--- src/router.tsx
+++ src/router.tsx
@@ -6,6 +6,7 @@ import Unusual from "./layouts/Unusual"
 import Homepage from "./pages/Homepage"
 import NotFound from "./pages/NotFound"
 import Login from "./pages/Login"
+import Register from "./pages/Register"
 
 export const router = createBrowserRouter([
   {
@@ -25,6 +26,10 @@ export const router = createBrowserRouter([
       {
         path: "/login",
         Component: Login,
+      },
+      {
+        path: "/register",
+        Component: Register,
       },
     ],
   },

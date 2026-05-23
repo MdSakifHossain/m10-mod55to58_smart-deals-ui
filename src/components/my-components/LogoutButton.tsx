296cd6b92a06e5d746389e5820a7ca45982a9033
MdSakifHossain <sakif015sakif@gmail.com>
2026-05-23 23:49:24 +0600

    feat(logout): add redirect logic on logout button

--- src/components/my-components/LogoutButton.tsx
+++ src/components/my-components/LogoutButton.tsx
@@ -1,12 +1,20 @@
+// @ts-nocheck
 import { Button } from "@/components/ui/button"
 import { useAuth } from "@/contexts/AuthProvider"
 import { IconRun } from "@tabler/icons-react"
+import { useNavigate } from "react-router"
 
 const LogoutButton = () => {
   const { logOutUser } = useAuth()
+  const navigate = useNavigate()
+
+  const handleButtonPress = () => {
+    logOutUser()
+    navigate("/")
+  }
 
   return (
-    <Button variant="destructive" onClick={() => logOutUser()}>
+    <Button variant="destructive" onClick={() => handleButtonPress()}>
       Log Out
       <IconRun stroke={2} />
     </Button>

9419943b8680ec841b9402b5dc317ad48be7c22d
MdSakifHossain <sakif015sakif@gmail.com>
2026-05-24 02:11:29 +0600

    feat(profile): add profile card with logout and mock delete account button on profile page

---
+++ src/lib/getRandomAvatar.ts
@@ -0,0 +1,171 @@
+export const default_avatars = [
+    {
+      id: 0,
+      url: "https://lh3.googleusercontent.com/dMI6qLoLXh9OD8heJly23vdqRJ25sToSfT04-j5LsxJUz2fSIDjiwlt-j07NVSB5uqs=cc",
+    },
+    {
+      id: 1,
+      url: "https://lh3.googleusercontent.com/t0kFqc801aR_uP8J7JAqRlN2PAhouSkYwWSORsIM4JI3IgIfb4bE3R31LaLIux9sPpVr=cc",
+    },
+    {
+      id: 2,
+      url: "https://lh3.googleusercontent.com/1igbE5ZPJNdkMKURM8dAPmKwiZY4ukOPSg4viDCQZKirHlo0IeSBpeRXvDhcB7phGoo=cc",
+    },
+    {
+      id: 3,
+      url: "https://lh3.googleusercontent.com/JuBxIlxgGFKxGgaPfGOG9-SE0G6vB82G7nK_LL4pS4eqPzabCjry-b5UzpHfl-KIsTjwKw=cc",
+    },
+    {
+      id: 4,
+      url: "https://lh3.googleusercontent.com/natAaTJqH8o8n952ui0MAyA6SfZCN5izU4l9iLvC7pvaE6ysFmDepyq7RP7OyPm8Fnk=cc",
+    },
+    {
+      id: 5,
+      url: "https://lh3.googleusercontent.com/n2E1SEZhkLSgYX0vzRtXvUrc5D4DtNZKj4EFMX_Bk_-yY5Da9HKb4L2iG7tjajZY8RVr=cc",
+    },
+    {
+      id: 6,
+      url: "https://lh3.googleusercontent.com/5sKNBO62RREO3T5ZMc0JuP0EVj7eL24X_LMaQgMznTj6NEG2fvKm8eSE-i-bq2cHqyM=cc",
+    },
+    {
+      id: 7,
+      url: "https://lh3.googleusercontent.com/tQh13vhR8Ll6dFcjCzc_hsniMR9z9ez5e3RM2uEriqbn0WdXl65zbi28g5zXhVD5RsJU=cc",
+    },
+    {
+      id: 8,
+      url: "https://lh3.googleusercontent.com/4xKyR2m7cv2i3eMq-4B8tZpud0785b5jyHmQE7zLYE84ifQsAwWLyb6BWD_0OaA0Kow=cc",
+    },
+    {
+      id: 9,
+      url: "https://lh3.googleusercontent.com/63DeYxR8bq_gycRthNjXINo3viD3hvcTxVRTADk3itX7-tNsX1Zicw_nJ24y5ZiBr5A=cc",
+    },
+    {
+      id: 10,
+      url: "https://lh3.googleusercontent.com/AyPiUG319eZ-sC7H59wqPL-z9UHfZ5SzEdsWCJSbL4rteBYnZRo6Ad8tT2gMBS_LBa1X=cc",
+    },
+    {
+      id: 11,
+      url: "https://lh3.googleusercontent.com/D38QLkZr1eEE1v7lrzUQxSJjAN2uhCoFeE4K-IVoZ8UjIaYknbilorEwKfqcoaiIgXOC=cc",
+    },
+    {
+      id: 12,
+      url: "https://lh3.googleusercontent.com/oAFG8zGwzdySbb7Z3sWNHv9Sel3L5JrX8-zNOQlxylLvL6ezr0o3eECaRcc5rmfzz_c=cc",
+    },
+    {
+      id: 13,
+      url: "https://lh3.googleusercontent.com/iHjqbKwh3ZQyDlqZpaEZdNr6gsfc0pS_QOh9LtQ1Q1cVb4jn3y7YXVQbOo8xs35cZyU=cc",
+    },
+    {
+      id: 14,
+      url: "https://lh3.googleusercontent.com/cShOhrPhqSPCO7z3yyuEOxkD9Deq0tUP3mTjnTeGm6gU7ThBa28JHDxciZOsgA4R0WCH=cc",
+    },
+    {
+      id: 15,
+      url: "https://lh3.googleusercontent.com/MHEv4AJrWEsEfB3Ont4tcv8BokNtVSJNR-hw9WT4na1o22ZyEYUChxl5DGgYrF57DTr2=cc",
+    },
+    {
+      id: 16,
+      url: "https://lh3.googleusercontent.com/OLwbCZCCDJLhDxaxLnxCYQBfXzsm8tBknB7NftwoInh8WkHhfTTQ0GWP6cqlyAMjrmrL=cc",
+    },
+    {
+      id: 17,
+      url: "https://lh3.googleusercontent.com/XGMGc4QzvOYO06MXEh2VhtLMWSN3tCgjHmJw3hAokHawdxMfjzghnUIbp0Feq4YPsy7m=cc",
+    },
+    {
+      id: 18,
+      url: "https://lh3.googleusercontent.com/3NjXZV4b4ls0is6MQKk4Bokpofws1PhFCWQrlk_HJGR8SMg1T3JF1-Sro-BvE5Qz09QtpQ=cc",
+    },
+    {
+      id: 19,
+      url: "https://lh3.googleusercontent.com/aXMhiihL6APfs6-R_1qPXljKRk9-7EhZER4xqLQlhm4gSouV2vRrEvbo2UPbE9pB7wo=cc",
+    },
+    {
+      id: 20,
+      url: "https://lh3.googleusercontent.com/kFNyY-tHFLJsDMYSbK0bqPLXSePk5ExR2jdCCQXpzF8egcgyfce3MAAvksiKyfjtIVur=cc",
+    },
+    {
+      id: 21,
+      url: "https://lh3.googleusercontent.com/oX1IqLRZT_hUKBoGRBQdgSifTNXq2C8D088WX-sadJnW5oGyUTW8cqB37OFdKT-ZWg32zQ=cc",
+    },
+    {
+      id: 22,
+      url: "https://lh3.googleusercontent.com/7I3nsBwpQRWYPeKGICCee3jn99jy_YMFumnRUnbQIPfkp7wT72G1gFqU6uGrIiQbMy8=cc",
+    },
+    {
+      id: 23,
+      url: "https://lh3.googleusercontent.com/oSr9gh739dEkrcraGq9qXOOEbfNCznrRfKqeibAHEyIdCLHB-EQFDot-00Z4TsMAjRE=cc",
+    },
+    {
+      id: 24,
+      url: "https://lh3.googleusercontent.com/mVGjeQUdZJI0CXJHbz2yO0_gnxQiQu6Bo-KAtqQ5bHUXbZ2UrSdHBd0vmb2Qw2Vnn714=cc",
+    },
+    {
+      id: 25,
+      url: "https://lh3.googleusercontent.com/TLYh2dKsySycir7dbpOz4rnM6kKvMIapi3nW25k2_UYkmoMSsOqnWmjEUhgNL1g9Pb8=cc",
+    },
+    {
+      id: 26,
+      url: "https://lh3.googleusercontent.com/Xxy9_NiWFu_ww6Fy4fFrmc3CzJDUrgeOOZQpEVE2S2uOeMHhoWLOD5t0-SLocnm--Mvo=cc",
+    },
+    {
+      id: 27,
+      url: "https://lh3.googleusercontent.com/nKDUvCrPnYqQrN2EfKrGNOjxpQD0SVWtdaLGSNhH9iqQu02X-PT16NRq-jXqXOxdgNfjCw=cc",
+    },
+    {
+      id: 28,
+      url: "https://lh3.googleusercontent.com/WCH2rV2GnLbum4sEIuGFeI8Q-ZpA1jHil6rgXPjqf2tl9uiHDiC0KUPCaY-MyR0UnmMm=cc",
+    },
+    {
+      id: 29,
+      url: "https://lh3.googleusercontent.com/xxmRatYZEZK_oMeoRY7neeZfp22e96x8GhyAKlMv1OUA-chYm44LacMwF8g4K_45DrzC=cc",
+    },
+    {
+      id: 30,
+      url: "https://lh3.googleusercontent.com/cRsHHCBc32h7DrsAhPAfOzb6duHcUGfSO6zRpfAjkn2C2PF25bMTdc8V7hZKYSC2N4um=cc",
+    },
+    {
+      id: 31,
+      url: "https://lh3.googleusercontent.com/r6xqj2d6DbmthPOYOnf0zy-tx2rAmGfQKnzpQYtAdmroWLoN_fjhvqL-CjTad1Oyi-dFdg=cc",
+    },
+    {
+      id: 32,
+      url: "https://lh3.googleusercontent.com/5UFIty4MDtZvr1TvEJ3l4ikh5kpTSnuNwYOoC2bye6ybxqMAdOa7i_kS3hq5lZdn-yg=cc",
+    },
+    {
+      id: 33,
+      url: "https://lh3.googleusercontent.com/7LMytAwYiCR3WkBCJ0ndMRSFmDkC6X22IY6uN4kr8fwFAJags3sVC5nvWBxtugvpwwF2=cc",
+    },
+    {
+      id: 34,
+      url: "https://lh3.googleusercontent.com/i61MD5dO61eeNnQAXRMqASmwJXiRmQ0_dpJe38wQ1JhjV2FtTdhBpZoM6F7mSOGrd-IMqw=cc",
+    },
+    {
+      id: 35,
+      url: "https://lh3.googleusercontent.com/xRULGNBlh7Ep3oy5OSOkxAODZFYnE8M5BdEpOOIeSpGkzVxicDxRtHb0SmQ-kqUi9TM=cc",
+    },
+    {
+      id: 36,
+      url: "https://lh3.googleusercontent.com/JxZ96dQscedKCuZpCIG0jIlRzyW07vjrwlSdZ1_wrhgVHoofsVInRcDUd2_AodZRpvc=cc",
+    },
+    {
+      id: 37,
+      url: "https://lh3.googleusercontent.com/BDYz2mEEqtB9JGCH_uR0rE6lIIJOnoRhAzdla91I-sFg-cdl1NUi79AYciv5iujzL7711Q=cc",
+    },
+    {
+      id: 38,
+      url: "https://lh3.googleusercontent.com/xFIMthae1b7I5wcxWI6_8JQvBivrZqm038xazvQ3y9KN_crHQ69FkaufBpwB88lN3JL6Tg=cc",
+    },
+    {
+      id: 39,
+      url: "https://lh3.googleusercontent.com/7jq3Ph22bICUGsQn_M9AmSprl0hvLXKjsE6_Xehpo5coZ-dIYkMzzmz9w_e9z_YGCekP9w=cc",
+    },
+]
+
+const getRandom = (list) => {
+  const randomIndex = Math.floor(Math.random() * list.length);
+  return list[randomIndex];
+};
+
+export const getRandomAvatar = () => {
+    return getRandom(default_avatars)
+}
--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,11 +1,61 @@
+// @ts-nocheck
+import LogoutButton from "@/components/my-components/LogoutButton"
 import PageStructure from "@/components/my-components/PageStructure"
+import { Card, CardContent } from "@/components/ui/card"
+import { Skeleton } from "@/components/ui/skeleton"
+import { useAuth } from "@/contexts/AuthProvider"
+import { getRandomAvatar } from "@/lib/getRandomAvatar"
 
-const Profile = () => {
+export default function Profile() {
+  const { loading, user } = useAuth()
+
   return (
-    <PageStructure className="flex items-center justify-center border">
-      <div className="text-4xl">Profile Page</div>
+    <PageStructure className="flex flex-col items-center justify-center gap-4">
+      <UserProfile loading={loading} user={user} />
+      <div className="grid min-w-md grid-cols-2 gap-4">
+        <p className="rounded-md border px-1 py-1 text-sm">Delete Button</p>
+        <LogoutButton />
+      </div>
     </PageStructure>
   )
 }
 
-export default Profile
+function UserProfile({ loading, user }) {
+  if (loading) {
+    return (
+      <Card className="min-w-md">
+        <CardContent>
+          <div className="flex w-fit items-center gap-4">
+            <Skeleton className="size-24 shrink-0 rounded-full" />
+            <div className="grid gap-5">
+              <Skeleton className="h-6 w-40" />
+              <Skeleton className="h-5 w-44" />
+            </div>
+          </div>
+        </CardContent>
+      </Card>
+    )
+  }
+
+  return (
+    <Card className="min-w-md">
+      <CardContent>
+        <div className="flex w-fit items-center gap-4">
+          <img
+            src={user.photoURL || getRandomAvatar().url}
+            alt="Profile"
+            className="size-24 rounded-full border"
+          />
+          <div className="grid gap-2">
+            <h3 className="text-2xl font-semibold">
+              {user.displayName || `Null`}
+            </h3>
+            <p className="text-sm text-muted-foreground">
+              someone@something.test
+            </p>
+          </div>
+        </div>
+      </CardContent>
+    </Card>
+  )
+}

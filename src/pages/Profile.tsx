6cb00307c5e76ea69f77420d56e253a05d0de221
MdSakifHossain <sakif015sakif@gmail.com>
2026-05-26 18:45:47 +0600

    add conditional checks before showing fallback UI

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -39,16 +39,16 @@ function UserProfile({ loading, user, profileUrl }) {
       <CardContent>
         <div className="flex w-fit items-center gap-4">
           <img
-            src={user.photoURL || profileUrl}
+            src={user?.photoURL || profileUrl}
             alt="Profile"
             className="size-24 rounded-full border"
           />
           <div className="grid gap-2">
             <h3 className="text-2xl font-semibold">
-              {user.displayName || `Null`}
+              {user?.displayName || `Null`}
             </h3>
             <p className="text-sm text-muted-foreground">
-              someone@something.test
+              {user?.email || "Null"}
             </p>
           </div>
         </div>

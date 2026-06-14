3d7d96653a3cf68743c0c608668157e141a0f8da
MdSakifHossain <sakif015sakif@gmail.com>
2026-06-14 20:54:43 +0600

    fix(profile): show DisplayName && email of the logged in user

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -39,16 +39,16 @@ function UserProfile({ loading, user, profileUrl }) {
       <CardContent>
         <div className="flex w-fit items-center gap-4">
           <img
-            src={user?.photoURL || profileUrl}
+            src={user?.firebaseUser?.photoURL || profileUrl}
             alt="Profile"
             className="size-24 rounded-full border"
           />
           <div className="grid gap-2">
             <h3 className="text-2xl font-semibold">
-              {user?.displayName || `Null`}
+              {user?.firebaseUser?.displayName || `Null`}
             </h3>
             <p className="text-sm text-muted-foreground">
-              {user?.email || "Null"}
+              {user?.firebaseUser?.email || "Null"}
             </p>
           </div>
         </div>
